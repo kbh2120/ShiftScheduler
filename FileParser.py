498dfe1465ceb4d45cc3a6ec21ef9c1d664fe1ea
Kristie Howard <kbh2120@columbia.edu>
2015-04-21 15:50:03 -0400

    started work on FileParser and created files for each

---
+++ FileParser.py
@@ -0,0 +1,23 @@
+
+class FileParser:
+
+	def __init__(self, inFile):
+		self.inFile = inFile
+		self.prob = SSProblem()
+
+	def readFile():
+		
+	
+	# create Person 
+	def createVariable(variable):
+
+	# create Shift
+	def createConstraint(constraint):
+
+
+	def createSSProblem():
+		self.readFile()
+
+
+		return self.prob
+
---
+++ SSProblem.py
@@ -0,0 +1,10 @@
+
+class SSProblem:
+	def __init__(self):
+
+	# adds a constraint to a given var object 
+	def addConstraint(variable, constraint):
+	
+	# adds a variable
+	def addVariable(variable):
+	
