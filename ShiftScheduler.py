0550b5cc8ca47917ddf5bb1fedda717089dadff4
Sandya Sankarram <ss4119@columbia.edu>
2015-05-06 19:46:21 -0400

    connect gui with FileParser

--- ShiftScheduler.py
+++ ShiftScheduler.py
@@ -4,17 +4,46 @@
 ###############################################################################
 
 from FileParser import *
+from Tkinter import *
 
 
 class ShiftScheduler: 
-	def __init__(self):
+	def __init__(self, path, out):
 		#global shift array, must be passed into fileparser
 		self.SHIFTS = []
-		fp = FileParser('./sample_data.csv', self.SHIFTS)
+		fp = FileParser(path, self.SHIFTS)
 		fp.read_file()
+		fp.create_SSProblem()
+
+def callback(csv_path, out_path):
+	ss = ShiftScheduler(csv_path.get(), out_path.get())
 
 def main():
-	ss = ShiftScheduler()
+	master = Tk()
+	master.minsize(300,300)
+	master.geometry("500x500")
 
+	fp = Label(master, text="Enter full csv path: ")
+	fp.pack()
+	csv_path = Entry(master)
+	csv_path.pack()
+	csv_path.focus_set()
+
+	op = Label(master, text="Enter full outfile path: ")
+	op.pack()
+	out_path = Entry(master)
+	out_path.pack()
+	out_path.focus_set()
+	b = Button(master, text="Schedule Us!", width=30, height=20, command=lambda: callback(csv_path, out_path))
+	b.pack()
+	mainloop()
 
 main()
+
+
+
+
+
+
+
+
