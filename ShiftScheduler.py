6ebdc1062e8d6a50a00116bf475e3ec2c59f6fcb
Kristie Howard <kbh2120@columbia.edu>
2015-05-06 13:22:59 -0400

    added Name attribute to people if First Name and Last Name exist

--- ShiftScheduler.py
+++ ShiftScheduler.py
@@ -10,8 +10,8 @@ class ShiftScheduler:
 	def __init__(self):
 		#global shift array, must be passed into fileparser
 		self.SHIFTS = []
-		# fp = FileParser('./sample_data.csv', self.SHIFTS)
-		fp = FileParser('/Users/kristiehoward/Desktop/my_form_responses.csv', self.SHIFTS)
+		fp = FileParser('./sample_data.csv', self.SHIFTS)
+		#fp = FileParser('/Users/kristiehoward/Desktop/my_form_responses.csv', self.SHIFTS)
 		fp.create_SSProblem()
 
 def main():
