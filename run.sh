6ced54b89e106bdac2e497c083d5403f888dc1ea
Kristie Howard <kbh2120@columbia.edu>
2015-05-08 12:04:55 -0400

    added logging to run script

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
+echo "*******************************************"
+echo "Now installing necessary packages"
+echo "*******************************************"
+# need password to install logilab
 sudo chown -R $USER /Library/Python/2.7
 cd logilab-constraint-0.5.0
+# install logilab
 python setup.py install
 cd ..
+# install pandas
 pip install pandas
+# run program
+echo "*******************************************"
+echo "Please look for a tkinter application (the icon is a feather)"
+echo "*******************************************"
 python ShiftScheduler.py
+echo "*******************************************"
+echo "Please look for your results in the file you specified."
+echo "*******************************************"
